9e1ab9cadf18f7f87735266ecf38911e2fc06e50
joshsalazar <joshsalazar@192.168.0.13>
2018-09-18 22:25:25 -0600

    Creating the class controller for the MVC architecture.

---
+++ POC/Controller.py
@@ -0,0 +1,38 @@
+import Cleaner
+from LoadImages import Image
+from LoadCSV import CSV
+from LoadModel import Model
+import os
+
+class Controller:
+    _folder = "/images/"
+    
+    def __init__(self):
+        self._project_path = os.path.dirname(os.path.abspath(__file__))
+        self._images_path = ""
+        self._list_images = []
+        
+    def create_folder(self):
+        self._images_path = self._project_path+self._folder
+        if not os.path.isdir(self._images_path):   #Search if there is a directory to save images uploaded
+            os.mkdir(self._images_path)
+            #print("Could create directory: {}.".format(target))
+    
+    def _list_files(self, folder):
+        return os.listdir(folder)
+    
+    def create_Image(self, img):
+        image = Image(self._images_path, img)
+        self._list_images.append(image)
+    
+    @property
+    def images_path(self):
+        return self._images_path
+    
+    @property
+    def list_images(self):
+        return self._list_files(self._project_path+os.path.abspath(self._folder))
+    
+    @list_images.setter
+    def list_images(self, list_images):
+        self._list_images = list_images
